e9c5f355f39ef9a459a3ac80cac32e82dbeb13e3
Samuelcodingg <samuelroman39@gmail.com>
2021-10-08 11:28:00 -0500

    important packages was added

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT;
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
 
 app.listen(port, () => {
     console.log(`App is listening at port ${port}`);
